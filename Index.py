ca548217e98734c31ddc12db28d932ae70a288ae
ExploitDevX <Muziwandilem7@gmail.com>
2025-03-17 00:18:22 +0200

    Updated Index.py

--- Index.py
+++ Index.py
@@ -1,12 +1,11 @@
 numbers = 420 + 200 
 print(numbers)
-620
+
 numbers1 = 325 - 200
 print(numbers1)
-125
+
 numbers2 = 720 / 360
 print(numbers2)
-2.0
+
 numbers3 = 510 * 947
 print(numbers3)
-482970
